553e3067d28b7c6cbf1754a2fceffb334b2dee26
radpac <195882@edu.p.lodz.pl>
2018-05-22 18:38:35 +0200

    Changing structure

---
+++ src/main/java/com/radpac/CryptoChecker/CryptoCheckerApplication.java
@@ -0,0 +1,12 @@
+package com.radpac.CryptoChecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoCheckerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/radpac/CryptoChecker/CryptoCheckerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.radpac.CryptoChecker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CryptoCheckerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
