e71df40df9a5964aa304edb184aa311ed003df68
radpac <195882@edu.p.lodz.pl>
2018-05-23 10:19:26 +0200

    Working Spring at port 8181

---
+++ src/main/java/com/radpac/CryptoChecker/HomeController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.radpac.CryptoChecker;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author radekpachla
+ */
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        return "Hello World!";
+    }
+    
+}
